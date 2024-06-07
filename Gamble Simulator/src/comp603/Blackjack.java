389dd4429790d97246d07e1ad80e5e0d19f9110e
Ckroma28 <155805492+Ckroma28@users.noreply.github.com>
2024-06-07 23:31:15 +1200

    Blackjack game update
    - Added Card Objects
    - Integrated Deck and Blackjack class methods
    - Added get methods for User and Dealer hands
    - Added "Hit" and "Stand" buttons for Blackjack GUI game
    - Added a counter for card hand value

--- Gamble Simulator/src/comp603/Blackjack.java
+++ Gamble Simulator/src/comp603/Blackjack.java
@@ -24,94 +24,7 @@ public class Blackjack {
         this.balance = loadedUser.getBalance();
         scanner = new Scanner(System.in);
     }
-
-    public void play() {
-        Scanner scanner = new Scanner(System.in);
-        boolean playAgainLoop = true;
-
-        System.out.println("Welcome to Blackjack!");
-        explanation();
-
-        while (playAgainLoop) {
-            deck.shuffle();
-            resetGame();
-            System.out.println("Your balance is: $" + balance);
-
-            double betAmount = 0;
-            boolean validBet = false;
-
-            do {
-                try {
-                    System.out.println("Enter your bet amount:");
-                    betAmount = scanner.nextDouble();
-                    scanner.nextLine();
-
-                    if (betAmount <= 0 || betAmount > balance) {
-                        System.out.println("Invalid bet amount. Please enter a bet amount between 1 and your balance.");
-                    } else {
-                        validBet = true;
-                    }
-                } catch (InputMismatchException e) {
-                    System.out.println("Invalid input. Please enter a valid numeric bet amount.");
-                    scanner.nextLine();
-                }
-            } while (!validBet);
-
-            dealStartingHand();
-            printHands(true);
-
-            while (true) {
-                System.out.println("Hit or Stand? (h/s): ");
-                String choice = scanner.nextLine();
-
-                if (choice.equalsIgnoreCase("h")) {
-                    hit(userHand);
-                    printHands(true);
-                    System.out.println(countHandValue(userHand));
-
-                    if (countHandValue(userHand) > 21) {
-                        System.out.println("Bust! Dealer wins.");
-                        balance -= betAmount;
-
-                        if (!askPlayAgain(scanner)) {
-                            resetGame();
-                            playAgainLoop = false;
-                        }
-                        break;
-                    }
-                } else if (choice.equalsIgnoreCase("s")) {
-                    while (countHandValue(dealerHand) < 17) {
-                        hit(dealerHand);
-                    }
-                    boolean win = determineWinner();
-                    printHands(false);
-                    System.out.println("Dealer's hand: " + dealerHand);
-
-                    if (win) {
-                        System.out.println("You win!");
-                        balance += betAmount;
-                        UserFileHandler.updateUserData(user, balance);
-                    } else if (!win && countHandValue(userHand) == countHandValue(dealerHand)) {
-                        System.out.println("Tie!");
-                    } else {
-                        System.out.println("You lose!");
-                        balance -= betAmount;
-                        UserFileHandler.updateUserData(user, balance);
-                    }
-
-                    System.out.println("Your balance is: $" + balance);
-                    if (!askPlayAgain(scanner)) {
-                        playAgainLoop = false;
-                    }
-                    resetGame();
-                    break;
-                } else {
-                    System.out.println("Invalid choice! Please enter 'h' or 's'.");
-                }
-            }
-        }
-    }
-
+    
     public void dealStartingHand() {
         userHand.add(deck.draw());
         userHand.add(deck.draw());
@@ -119,19 +32,19 @@ public class Blackjack {
         dealerHand.add(deck.draw());
     }
 
-    private void hit(List<String> hand) {
+    public List<String> getUserHand() {
+        return userHand;
+    }
+
+    public List<String> getDealerHand() {
+        return dealerHand;
+    }
+
+    public void hit(List<String> hand) {
         hand.add(deck.draw());
     }
 
-    private void printHands(boolean showDealer) {
-        System.out.println("Your hand: " + userHand + " ");
-
-        if (showDealer) {
-            System.out.println("Dealer's hand: [" + dealerHand.get(0) + ", (?)]");
-        }
-    }
-
-    private int countHandValue(List<String> hand) {
+    public int countHandValue(List<String> hand) {
         int count = 0;
         int aceCount = 0;
 
@@ -167,44 +80,9 @@ public class Blackjack {
             return false;
         }
     }
-    
+
     public void resetGame() {
         userHand.clear();
         dealerHand.clear();
-    }
-    
-    public boolean askPlayAgain(Scanner scanner) {
-        System.out.println("\nDo you want to play again? (yes/no)");
-        String playAgain;
-        do {
-            playAgain = scanner.nextLine().trim().toLowerCase();
-            if (playAgain.equals("yes") || playAgain.equals("no")) {
-                break;
-            } else {
-                System.out.println("Invalid input! Please enter 'yes' or 'no'.");
-            }
-        } while (true);
-        return playAgain.equals("yes");
-    }
-    
-    private void explanation() {
-        System.out.println("Would you like an explanation on how this game works? (yes/no)");
-        String response = scanner.nextLine().toLowerCase();
-        if (response.equals("yes")) {
-            System.out.println("The goal of blackjack is to get your count closest to 21.");
-            System.out.println("The user and dealer both get 2 cards each.");
-            System.out.println("The user must get a higher count than the dealer to win.");
-            System.out.println("If the user has a smaller count value than the dealer they lose.");
-            System.out.println("Each card is worth what the rank is (e.g. 9 is worth 9 counts)");
-            System.out.println("Cards such as Jack, Queen or King have a count value of 10.");
-            System.out.println("Ace cards work differently.");
-            System.out.println("If a hand with 2 cards and one of those cards is an Ace,");
-            System.out.println("The Ace is worth 11 counts. If a hand has more than 2 cards,");
-            System.out.println("The Ace is worth 1 count.\n");
-        } else if (response.equals("no")) {
-            System.out.println("Alright, good luck!");
-        } else {
-            System.out.println("Invalid input, continuing with game...");
-        }
     }
 }
---
+++ Gamble Simulator/src/comp603/GameSelection.java
@@ -0,0 +1,196 @@
+package comp603;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
+
+public class GameSelection extends javax.swing.JPanel {
+
+    private User user;
+
+    public GameSelection() {
+        initComponents();
+    }
+
+    public void updateUser(User user) {
+        this.user = user;
+        CurrentUserText.setText("User: " + user.getUsername());
+        CurrentBalanceText.setText("Balance: $" + user.getBalance());
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">                          
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        SlotsButton = new javax.swing.JButton();
+        CoinflipButton = new javax.swing.JButton();
+        BlackjackButton = new javax.swing.JButton();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        ChooseGameText = new javax.swing.JTextArea();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        CurrentBalanceText = new javax.swing.JTextArea();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        CurrentUserText = new javax.swing.JTextArea();
+        QuitButton = new javax.swing.JButton();
+
+        jPanel1.setMaximumSize(new java.awt.Dimension(1000, 500));
+        jPanel1.setMinimumSize(new java.awt.Dimension(1000, 500));
+        jPanel1.setPreferredSize(new java.awt.Dimension(1000, 500));
+
+        SlotsButton.setText("SLOTS");
+        SlotsButton.setMaximumSize(new java.awt.Dimension(200, 50));
+        SlotsButton.setMinimumSize(new java.awt.Dimension(200, 50));
+        SlotsButton.setPreferredSize(new java.awt.Dimension(200, 50));
+
+        CoinflipButton.setText("COINFLIP");
+        CoinflipButton.setMaximumSize(new java.awt.Dimension(200, 50));
+        CoinflipButton.setMinimumSize(new java.awt.Dimension(200, 50));
+        CoinflipButton.setPreferredSize(new java.awt.Dimension(200, 50));
+
+        BlackjackButton.setText("BLACKJACK");
+        BlackjackButton.setMaximumSize(new java.awt.Dimension(200, 50));
+        BlackjackButton.setMinimumSize(new java.awt.Dimension(200, 50));
+        BlackjackButton.setPreferredSize(new java.awt.Dimension(200, 50));
+        
+        BlackjackButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                BlackjackButtonActionPerformed(evt);
+            }
+        });
+
+        jScrollPane2.setHorizontalScrollBarPolicy(javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        jScrollPane2.setVerticalScrollBarPolicy(javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
+
+        ChooseGameText.setEditable(false);
+        ChooseGameText.setColumns(20);
+        ChooseGameText.setFont(new java.awt.Font("Segoe UI", 0, 36)); // NOI18N
+        ChooseGameText.setRows(1); 
+        ChooseGameText.setText("Please choose a game");
+        ChooseGameText.setMinimumSize(new java.awt.Dimension(600, 60)); 
+        ChooseGameText.setPreferredSize(new java.awt.Dimension(600, 60));
+        jScrollPane2.setViewportView(ChooseGameText);
+
+        jScrollPane1.setHorizontalScrollBarPolicy(javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        jScrollPane1.setVerticalScrollBarPolicy(javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
+
+        CurrentBalanceText.setEditable(false);
+        CurrentBalanceText.setColumns(20);
+        CurrentBalanceText.setRows(1); 
+        CurrentBalanceText.setText("Balance: ");
+        CurrentBalanceText.setMaximumSize(new java.awt.Dimension(260, 40));
+        CurrentBalanceText.setMinimumSize(new java.awt.Dimension(260, 40));
+        CurrentBalanceText.setPreferredSize(new java.awt.Dimension(260, 40)); 
+        jScrollPane1.setViewportView(CurrentBalanceText);
+
+        jScrollPane3.setHorizontalScrollBarPolicy(javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        jScrollPane3.setVerticalScrollBarPolicy(javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
+
+        CurrentUserText.setEditable(false);
+        CurrentUserText.setColumns(20);
+        CurrentUserText.setRows(1); 
+        CurrentUserText.setText("User: ");
+        CurrentUserText.setMaximumSize(new java.awt.Dimension(260, 40));
+        CurrentUserText.setMinimumSize(new java.awt.Dimension(260, 40)); 
+        CurrentUserText.setPreferredSize(new java.awt.Dimension(260, 40)); 
+        jScrollPane3.setViewportView(CurrentUserText);
+
+        QuitButton.setText("QUIT");
+        QuitButton.setMaximumSize(new java.awt.Dimension(200, 50));
+        QuitButton.setMinimumSize(new java.awt.Dimension(200, 50));
+        QuitButton.setPreferredSize(new java.awt.Dimension(200, 50));
+        
+        QuitButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                QuitButtonActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(50, 50, 50)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 600, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(SlotsButton, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(CoinflipButton, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(BlackjackButton, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(QuitButton, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(150, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(50, 50, 50)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE) 
+                .addGap(50, 50, 50)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(SlotsButton, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(CoinflipButton, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(BlackjackButton, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(QuitButton, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(150, Short.MAX_VALUE))
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+    }// </editor-fold>    
+    
+    private void QuitButtonActionPerformed(java.awt.event.ActionEvent evt) {
+        JOptionPane.showMessageDialog(this, "Thanks for Playing!");
+        System.exit(0);
+    }
+    
+    private void BlackjackButtonActionPerformed(ActionEvent evt) {
+        BlackjackGUI blackjackGUI = new BlackjackGUI();
+
+        JFrame topFrame = (JFrame) SwingUtilities.getWindowAncestor(this);
+        topFrame.getContentPane().removeAll();
+        topFrame.add(blackjackGUI);
+        topFrame.validate();
+        topFrame.repaint();
+    }
+
+
+    // Variables declaration - do not modify                     
+    private javax.swing.JButton BlackjackButton;
+    private javax.swing.JTextArea ChooseGameText;
+    private javax.swing.JButton CoinflipButton;
+    private javax.swing.JTextArea CurrentBalanceText;
+    private javax.swing.JTextArea CurrentUserText;
+    private javax.swing.JButton QuitButton;
+    private javax.swing.JButton SlotsButton;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
+    // End of variables declaration                   
+}
